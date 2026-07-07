348ac5fbec465b5825a6d988db0f22e2bb366c14
M Umar Habib <100488355+umarhabib30@users.noreply.github.com>
2026-07-07 16:45:19 +0500

    Add helper functions inclusion in AppServiceProvider for improved utility access

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        require_once app_path('Support/helpers.php');
     }
 
     /**
